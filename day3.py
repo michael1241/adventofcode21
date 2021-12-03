faa6da6db935f297c2725fa5d373d66654bbbe7f
michael1241 <michael@jamcupboard.co.uk>
2021-12-03 11:35:16 +0000

    day3 complete

--- day3.py
+++ day3.py
@@ -1,13 +1,47 @@
 #! /usr/bin/env python3
 
 import numpy as np
-from scipy.stats import mode
 
 with open('./day3in') as f:
-    lines = list(map(list, f.read().splitlines()[:-1]))
+    lines = np.array(list(map(list, f.read().splitlines()))).astype(int)
 
-gamma_array = mode(np.array(lines).astype(int)).mode[0]
-gamma = ''.join(str(i) for i in gamma_array)
-epsilon = ''.join(['0' if x == '1' else '1' for x in gamma])
 
-print(int(gamma, 2) * int(epsilon, 2))
+def modeAndAnti(arr):
+    inv = np.where((arr == 0) | (arr == 1), arr ^ 1, arr)
+    anti = inv.mean(axis=0).round().astype(int)
+    m = np.where((anti == 0) | (anti == 1), anti ^ 1, anti)
+    return m, anti
+
+
+def arrayToInt(arr):
+    return int(''.join(arr.astype(str)), 2)
+
+
+def one(l):
+    gamma, epsilon = modeAndAnti(l)
+    return gamma, epsilon
+    # return arrayToInt(gamma) * arrayToInt(epsilon)
+
+
+def filt(arr, pos, value):
+    return arr[(arr[:, pos] == value)]
+
+
+def two(l):
+    oxy = l.copy()
+    co2 = l.copy()
+
+    oxy_gam, co2_gam = modeAndAnti(l)
+
+    for pos in range(len(l[0])):
+        if len(oxy) != 1:
+            oxy = filt(oxy, pos, oxy_gam[pos])
+            oxy_gam, _ = modeAndAnti(oxy)
+        if len(co2) != 1:
+            co2 = filt(co2, pos, co2_gam[pos])
+            _, co2_gam = modeAndAnti(co2)
+
+    return arrayToInt(oxy[0]) * arrayToInt(co2[0])
+
+
+print(two(lines))

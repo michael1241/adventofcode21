42728de179330341c7c5f38157d8d00d5613c7c4
michael1241 <michael@jamcupboard.co.uk>
2021-12-07 14:09:39 +0000

    refactor to use mean

--- day7.py
+++ day7.py
@@ -1,25 +1,29 @@
 #! /usr/bin/env python3
 
-from statistics import median
+from statistics import median, mean
+from math import ceil, floor
 
 crabs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 
 with open('./day7in') as f:
     crabs = [int(x) for x in f.read().split(',')]
 
-med = median(crabs)  # used for part 1 instead of pos
+med = median(crabs)  # used for part 1
+avg = mean(crabs)
 
 
 def triangle(n):
     return int(n * (n + 1) / 2)
 
 
-min_fuel = 100000000000  # large number
+avgs = (floor(avg), ceil(avg))
 
-for pos in range(min(crabs), max(crabs) + 1):
+
+def checkFuel(avg):
     fuel = 0
     for crab in crabs:
-        fuel += triangle(abs(crab - pos))
-    min_fuel = min(min_fuel, fuel)
+        fuel += triangle(abs(crab - avg))
+    return fuel
 
-print(min_fuel)
+
+print(min((checkFuel(avg) for avg in avgs)))

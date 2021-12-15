1ba802d9df05f2d79bcd01f338561c198195bef9
michael1241 <michael@jamcupboard.co.uk>
2021-12-15 08:29:10 +0000

    day 14 part 2

--- day14.py
+++ day14.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 from more_itertools import pairwise, interleave_longest
-from collections import Counter
+from collections import Counter, defaultdict
 
 with open('day14in') as f:
     data = f.read().splitlines()
@@ -10,13 +10,37 @@ polymer, rules = data[0], data[2:]
 rules = {a: b for a, b in [rule.split(' -> ') for rule in rules]}
 
 
-def one(polymer, rules):
-    for n in range(10):
-        newitems = [rules[a + b] for a, b in pairwise(polymer)]
-        polymer = list(interleave_longest(polymer, newitems))
+def one(p, r, n):
+    for _ in range(n):
+        newitems = [r[a + b] for a, b in pairwise(p)]
+        p = list(interleave_longest(p, newitems))
 
-    res = Counter(polymer).values()
+    res = Counter(p).values()
     return max(res) - min(res)
 
 
-print(one(polymer, rules))
+def two(p, r, n):
+    first = p[0]
+    last = p[-1]
+    counts = Counter(["".join(i) for i in pairwise(p)])
+    newcounts = defaultdict(int)
+    for _ in range(n):
+        for pair, count in counts.items():
+            newcounts[pair[0] + r[pair]] += count
+            newcounts[r[pair] + pair[1]] += count
+        counts = newcounts.copy()
+        newcounts = defaultdict(int)
+    doubletotals = defaultdict(int)
+    for pair, count in counts.items():
+        doubletotals[pair[0]] += count
+        doubletotals[pair[1]] += count
+    totals = {k: v // 2 for k, v in doubletotals.items()}
+    totals[first] += 1
+    totals[last] += 1
+    return totals
+
+
+# print(one(polymer, rules, 10))
+output = two(polymer, rules, 40).values()
+
+print(max(output) - min(output))
